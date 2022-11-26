c4d650ecd53eefa7cfb3ba7ab418f3472a37f393
Shivansh Saini <shivanshs9@gmail.com>
2022-11-27 01:23:58 +0530

    fx: fix multi db script

--- db/create-multi-db.sh
+++ db/create-multi-db.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-set -e
-set -u
+set -eu
 
 function create_user_and_database() {
-	local dbinfo=$1
-	IFS=":" read -r database owner password <<< "$dbinfo"
+    local dbinfo=$1
+    IFS=":" read -r database owner password <<< "$dbinfo"
 
-	echo "Creating user and database '$database' with user '$owner'."
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $owner IF NOT EXISTS;
-	    ALTER USER $owner WITH ENCRYPTED PASSWORD '$password';
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $owner;
+    echo "Creating user and database '$database' with user '$owner'."
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+        DO
+            \$do\$
+            BEGIN
+            IF NOT EXISTS ( SELECT FROM pg_roles  
+                            WHERE  rolname = '$owner') THEN
+                CREATE USER $owner;
+                ALTER USER $owner WITH ENCRYPTED PASSWORD '$password';
+            END IF;
+            END
+            \$do\$;
+        CREATE DATABASE $database;
+        GRANT ALL PRIVILEGES ON DATABASE $database TO $owner;
 EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_user_and_database $db
-	done
-	echo "Multiple databases created"
+    echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+    for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
+        create_user_and_database $db
+    done
+    echo "Multiple databases created"
 fi
 
