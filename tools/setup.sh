22937d0964c3bd544d2e88dbb5b47dc13df3ce5f
Shivansh Saini <shivanshs9@gmail.com>
2023-06-20 21:21:15 +0530

    fx: copy tools folder instead of symlink

--- tools/setup.sh
+++ tools/setup.sh
@@ -8,7 +8,8 @@ if [ $(id -u) -eq 0 ]; then
 fi
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-MANIFESTS_PATH=${MANIFESTS_PATH:-"/var/lib/rancher/k3s/server/manifests"}
+# default: /var/lib/rancher/k3s/server/manifests
+MANIFESTS_PATH=${MANIFESTS_PATH:-"/mnt/data1/k8s/k3s/server/manifests"}
 CHECKFILE=".k3sauto"
 
 CLEANUP=0
@@ -27,6 +28,5 @@ find "$SCRIPTPATH" -maxdepth 1 -type d ! -path . -print | while read path; do
   dir=$(basename $path)
 	echo "[$dir] Found helm chart CRD for k3s."
   $SUDO [ -d $MANIFESTS_PATH/$dir ] && $SUDO rm -r $MANIFESTS_PATH/$dir
-  $SUDO mkdir -p $MANIFESTS_PATH/$dir
-  (cd $path && $SUDO find . -type f ! -path ./$CHECKFILE -exec ln -f $path/{} $MANIFESTS_PATH/$dir/{} \;)
+  $SUDO cp -r $path $MANIFESTS_PATH/$dir
 done
