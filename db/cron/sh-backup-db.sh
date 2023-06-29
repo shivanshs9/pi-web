f78cb323f66619bdbabe12e7b269b4007b116540
Shivansh Saini <shivanshs9@gmail.com>
2023-06-29 18:01:16 +0530

    fix: pgdump connectivity issue due to labels

--- db/cron/sh-backup-db.sh
+++ db/cron/sh-backup-db.sh
@@ -7,9 +7,11 @@ USER=postgres
 NOW=$(date +%Y-%m-%d-%I-%M%p)
 
 # backup globals
+mkdir -p $backupdir/postgres_globals
 pg_dumpall -U $USER --globals-only | gzip > $backupdir/postgres_globals/$NOW.sql.gz
 
 # backup individual databases
 for db in $(psql -U $USER -t -c "select datname from pg_database where not datistemplate" | grep '\S' | awk '{$1=$1};1'); do
-   pg_dump -U $USER $db | gzip > $backupdir/$db/$NOW.sql.gz
+    mkdir -p $backupdir/$db
+    pg_dump -U $USER $db | gzip > $backupdir/$db/$NOW.sql.gz
 done
